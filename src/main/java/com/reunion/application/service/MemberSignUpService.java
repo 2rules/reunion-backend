7d15fc9ad520e8c9de69cc024ba532a51a480cb3
kyubum <lkb0938@gmail.com>
2025-07-26 16:47:47 +0900

    feat: 회원가입 API

--- src/main/java/com/reunion/application/service/MemberSignUpService.java
+++ src/main/java/com/reunion/application/service/MemberSignUpService.java
@@ -4,7 +4,6 @@ import com.reunion.adapter.out.persistence.MemberEntityMapper;
 import com.reunion.adapter.out.persistence.MemberJpaRepository;
 import com.reunion.application.port.in.SignUpUseCase;
 import com.reunion.domain.member.Member;
-import jakarta.transaction.Transactional;
 import lombok.RequiredArgsConstructor;
 import org.springframework.stereotype.Service;
 
@@ -16,7 +15,6 @@ public class MemberSignUpService implements SignUpUseCase {
     private final MemberEntityMapper memberEntityMapper;
 
     @Override
-    @Transactional
     public long signUp(Member member) {
         member.validate();
 
